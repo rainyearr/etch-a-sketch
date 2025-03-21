17d7cecf5df4e8f4d211e1a733c6b1c20f0fce66
Rainier Dimasacat <rainierdimasacat22@gmail.com>
2025-03-21 15:27:46 +0800

    Added the Grid with Styling

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-const container = document.getElementById("container");
+const grid = document.getElementById("grid");
 
-for (let i = 0; i < 16; i++) {
+for (let i = 0; i < 16 * 16; i++) {
   const newDiv = document.createElement("div");
-  newDiv.textContent = `Div ${i + 1}`;
-  newDiv.style.border = "1px solid black";
-  newDiv.style.padding = "10px";
-  newDiv.style.margin = "5px";
+  //   newDiv.textContent = `Div ${i + 1}`;
+  //   newDiv.style.border = "1px solid black";
+  //   newDiv.style.padding = "10px";
+  //   newDiv.style.margin = "5px";
+  newDiv.textContent = i + 1;
+  newDiv.classList.add("grid-item");
 
-  container.appendChild(newDiv);
+  grid.appendChild(newDiv);
 }
